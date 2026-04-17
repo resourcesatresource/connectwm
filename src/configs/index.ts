29c10d6c75012c7df1c8681623ae39932afe283a
Saurav Sanjay <resourcesatresource@gmail.com>
2026-04-17 11:44:52 +0000

    Update PROD url.

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -2,19 +2,19 @@ export const API = {
   BE: {
     BASE: {
       DEV: "http://localhost:3000/api",
-      PROD: "https://node-api-3m9u.onrender.com/api",
+      PROD: "https://node-api-452537150366.asia-south2.run.app/",
     },
     CUSTOMERS: {
       DEV: "http://localhost:3000/api/customers",
-      PROD: "https://node-api-3m9u.onrender.com/api/customers",
+      PROD: "https://node-api-452537150366.asia-south2.run.app/api/customers",
     },
     USERS: {
       DEV: "http://localhost:3000/api/users",
-      PROD: "https://node-api-3m9u.onrender.com/api/users",
+      PROD: "https://node-api-452537150366.asia-south2.run.app/api/users",
     },
     AUTH: {
       DEV: "http://localhost:3000/api/auth",
-      PROD: "https://node-api-3m9u.onrender.com/api/auth",
+      PROD: "https://node-api-452537150366.asia-south2.run.app/api/auth",
     },
   },
 };
